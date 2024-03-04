af9d499855da408a064eada600e33d93475122f3
Smawexi <1281722462@qq.com>
2024-03-05 03:02:21 +0800

    Update

--- pyevaljs3/runtime.py
+++ pyevaljs3/runtime.py
@@ -20,6 +20,7 @@ def get_node_env():
     node = os.environ.get('NODE_PATH') if os.environ.get('NODE_PATH') else os.environ.get('NODE')
     if not node:
         return "node"
+    return node
 
 
 class AbstractRuntime:
